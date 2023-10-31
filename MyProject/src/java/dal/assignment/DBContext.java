eae26311bf5436b6ebd6a6e4b6ff775393712be0
khangln04 <khanghhth@gmail.com>
2023-11-01 01:44:45 +0700

    Create DBContext.java
    link to database in MySQL

---
+++ MyProject/src/java/dal/assignment/DBContext.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal.assignment;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ *
+ * @author fpt
+ */
+public class DBContext {
+    protected Connection connection;
+    public DBContext()
+    {
+        try {
+            String url = "jdbc:sqlserver://KHANG\\SQLEXPRESS:1433;databaseName=FALL2023_Assignment";
+            String user = "se1763";
+            String pass = "12345678";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
